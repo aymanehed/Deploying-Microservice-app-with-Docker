fd5e201729c18a93494f4487cc9cd55a650a074c
aymanehed <93987581+aymanehed@users.noreply.github.com>
2023-12-14 15:58:14 +0000

    add gateway-service container

--- inventory-service/src/main/java/org/sid/inventoryservice/repository/ProductRepository.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository< Product, Long> {
 }
