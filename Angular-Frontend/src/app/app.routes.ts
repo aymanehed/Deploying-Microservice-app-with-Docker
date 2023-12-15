10c0dbacb2ab2e9fcffb390002cd8dc7c7a8a004
aymanehed <93987581+aymanehed@users.noreply.github.com>
2023-12-15 17:07:59 +0000

    Create customers,orders,products component with routes

--- Angular-Frontend/src/app/app.routes.ts
+++ Angular-Frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import {CustomersComponent} from "./customers/customers.component";
+import {OrdersComponent} from "./orders/orders.component";
+import {ProductsComponent} from "./products/products.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"customers",component:CustomersComponent},
+  {path:"orders",component:OrdersComponent},
+  {path:"products",component:ProductsComponent}
+
+
+];
---
+++ Angular-Frontend/src/app/customers/customers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  imports: [],
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent {
+
+}
