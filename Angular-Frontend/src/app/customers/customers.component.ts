71c05fea153a708736d8b3dbaf13f5b30041895f
aymanehed <93987581+aymanehed@users.noreply.github.com>
2023-12-16 19:37:01 +0000

    set up cors problem

--- Angular-Frontend/src/app/customers/customers.component.ts
+++ Angular-Frontend/src/app/customers/customers.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-customers',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.css'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit{
+  customers: any;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://localhost:8081/customers').subscribe({
+     next: data  => {
+       this.customers = data;
+     },
+     error: error => {
+       console.log( error);
+    }
+
+    })
+  }
+
 
 }
