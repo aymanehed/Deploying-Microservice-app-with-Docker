011a8b1ad7dc8c972ae3e60e1b8cf0d460815fb5
aymanehed <93987581+aymanehed@users.noreply.github.com>
2023-12-17 14:40:48 +0000

    create  default page Component & Angular in docker compose

--- Angular-Frontend/src/app/app.routes.ts
+++ Angular-Frontend/src/app/app.routes.ts
@@ -3,8 +3,10 @@ import {CustomersComponent} from "./customers/customers.component";
 import {OrdersComponent} from "./orders/orders.component";
 import {ProductsComponent} from "./products/products.component";
 import {HomeComponent} from "./home/home.component";
+import {DefaultComponent} from "./default/default.component";
 
 export const routes: Routes = [
+  {path:"",component:DefaultComponent},
   {path:"customers",component:CustomersComponent},
   {path:"orders",component:OrdersComponent},
   {path:"products",component:ProductsComponent},
---
+++ Angular-Frontend/src/app/default/default.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default',
+  standalone: true,
+  imports: [],
+  templateUrl: './default.component.html',
+  styleUrl: './default.component.css'
+})
+export class DefaultComponent {
+
+}
